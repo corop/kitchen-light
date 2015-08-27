e2538594f9eee42fc39b5b8954331e6c43eee6d0
Andrew Sergeev <corop@list.ru>
2015-08-27 14:43:14 +0300

    Create KitchenSwitchIR.ino

---
+++ KitchenSwitchIR.ino
@@ -0,0 +1,40 @@
+#define SENSOR_LEVEL 700
+#define SENSOR_PIN   A0
+#define RELAY_PIN    5
+#define BLUE_LED_PIN 2
+#define PASS_PERIOD  100
+#define PASS_COUNT   10
+
+bool lightOn = false;
+int counter = 0;
+int oldValue = 0;
+
+
+void setup() {
+  int oldValue = analogRead(SENSOR_PIN);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BLUE_LED_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(BLUE_LED_PIN, HIGH);
+}
+
+
+void loop() { 
+  int currentValue = analogRead(SENSOR_PIN);
+  if(currentValue < SENSOR_LEVEL && oldValue < SENSOR_LEVEL) {
+    counter++;
+    if(lightOn && (counter == PASS_COUNT)) {
+      digitalWrite(RELAY_PIN, LOW);
+      digitalWrite(BLUE_LED_PIN, LOW);      
+      lightOn = !lightOn;
+    } else if (!lightOn && (counter == PASS_COUNT)){
+      digitalWrite(RELAY_PIN, HIGH);
+      digitalWrite(BLUE_LED_PIN, HIGH);
+      lightOn = !lightOn;
+    }     
+  } else {
+    counter = 0;
+  }
+  oldValue = currentValue;
+  delay(PASS_PERIOD);  
+}
