3b88fffdb0ec2cf15f2967866835c55d21d41f8f
asergeev <asergeev@glosav.ru>
2015-08-27 14:50:23 +0300

    small fix & formatting

--- KitchenSwitchIR/KitchenSwitchIR.ino
+++ KitchenSwitchIR/KitchenSwitchIR.ino
@@ -11,7 +11,7 @@ int oldValue = 0;
 
 
 void setup() {
-  int oldValue = analogRead(SENSOR_PIN);
+  oldValue = analogRead(SENSOR_PIN);
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
@@ -19,22 +19,22 @@ void setup() {
 }
 
 
-void loop() { 
+void loop() {
   int currentValue = analogRead(SENSOR_PIN);
-  if(currentValue < SENSOR_LEVEL && oldValue < SENSOR_LEVEL) {
+  if (currentValue < SENSOR_LEVEL && oldValue < SENSOR_LEVEL) {
     counter++;
-    if(lightOn && (counter == PASS_COUNT)) {
+    if (lightOn && (counter == PASS_COUNT)) {
       digitalWrite(RELAY_PIN, LOW);
-      digitalWrite(BLUE_LED_PIN, LOW);      
+      digitalWrite(BLUE_LED_PIN, LOW);
       lightOn = !lightOn;
-    } else if (!lightOn && (counter == PASS_COUNT)){
+    } else if (!lightOn && (counter == PASS_COUNT)) {
       digitalWrite(RELAY_PIN, HIGH);
       digitalWrite(BLUE_LED_PIN, HIGH);
       lightOn = !lightOn;
-    }     
+    }
   } else {
     counter = 0;
   }
   oldValue = currentValue;
-  delay(PASS_PERIOD);  
+  delay(PASS_PERIOD);
 }
